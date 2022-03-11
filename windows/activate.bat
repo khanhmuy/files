74f464585da7bdf8228d1744b05d7a732e6c412b
khanhmuy <maikhanhhuy0608@outlook.com>
2022-03-11 15:23:58 +0700

    remember kids, it's always morally correct to pirate from big corps

---
+++ windows/activate.bat
@@ -0,0 +1,7 @@
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+slmgr /upk
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms s8.uk.to
+slmgr /ato
+exit
