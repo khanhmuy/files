62bb119f3a852bcc1956eb69b88fa2104f63c815
khanhmuy <maikhanhhuy0608@outlook.com>
2023-02-22 12:12:54 +0700

    update emojiDL.py

--- emojiDL.py
+++ emojiDL.py
@@ -1,10 +1,14 @@
 # Based on LillieH1000 (https://github.com/LillieH1000/)'s script
-import os, requests, json
+import os, sys, requests, json
 val = input("Enter Server ID: ")
 discord = "https://discordapp.com/api/v6/guilds/" + val
 r = requests.get(discord, headers={"Authorization":"YOUR_TOKEN_HERE"})
 d = json.loads(r.content)
-n = d['name']
+try:
+    n = d['name']
+except:
+    print('Invalid server ID!')
+    exit()
 t = ''.join(e for e in n if e.isalnum())
 print('Selected server:', t)
 if os.path.exists(t) == True:
@@ -14,6 +18,8 @@ else:
     os.mkdir(t)
     print('Choose file format for still emojis: \n1. PNG \n2. webp')
     choice = input("Enter choice: ")
+    total = len(d['emojis'])
+    count = 0
     for x in d['emojis']:
         m = x['name']
         p = ''.join(e for e in m if e.isalnum())
@@ -30,12 +36,14 @@ else:
             with open(name, "wb") as file:
                 response = requests.get(link)
                 file.write(response.content)
-                print('Downloaded', name)
+                count = count + 1
+                print('\rDownloaded', count, 'of', total, 'emojis', end='\r')
         if (x['animated'] == True):
             link = "https://cdn.discordapp.com/emojis/" + x['id'] + ".gif?quality=lossless"
             name = t + "/" + p + ".gif"
             with open(name, "wb") as file:
                 response = requests.get(link)
                 file.write(response.content)
-                print('Downloaded', name)
+                count = count + 1
+                print('\rDownloaded', count, 'of', total, 'emojis', end='\r')
     print("\nDone!")
