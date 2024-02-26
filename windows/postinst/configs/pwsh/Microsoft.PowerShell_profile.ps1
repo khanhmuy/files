fbf919fe870fa380cf81c0f97b8fb95ef340aa66
khanhmuy <maikhanhhuy0608@outlook.com>
2024-02-26 08:18:14 +0700

    move windows setup to seperate repo, move emojidl to seperate folder

--- windows/postinst/configs/pwsh/Microsoft.PowerShell_profile.ps1
+++
@@ -1,6 +0,0 @@
-Write-Output "gm/ga/ge/gn hmuy"
-oh-my-posh init pwsh --config $env:USERPROFILE\candy_custom.omp.json | Invoke-Expression
-function gh {
-	Set-Location $env:USERPROFILE\Documents\gh
-}
-Set-Alias -name ghub -value gh
